646f0e549dc6a61e79bd75908c12ae23f93e34d7
Stéphanie Kécia <118320951+StephanieKecia@users.noreply.github.com>
2024-04-12 14:40:16 -0300

    Add files via upload

---
+++ Projeto-CalculoLucro/Projeto-4/Entities/Department.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projeto_4.Entities
+{
+    internal class Department
+    {
+        public string Name { get; set; }
+
+        public Department ()
+        {
+
+        }
+
+        public Department (string name)
+        {
+            Name = name;
+        }
+
+    }
+}
---
+++ Projeto-CalculoLucro/Projeto-4/Entities/HourContract.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Projeto_4.Entities
+{
+    internal class HourContract
+    {
+        public DateTime Data { get; set; }
+        public double ValuePerHour { get; set; }
+        public int Hours { get; set;}
+
+        public HourContract() //construtor padrão
+        {
+
+        }
+
+        public HourContract(DateTime data, double valuePerHour, int hours)  //construtor
+        {
+            Data = data;
+            ValuePerHour = valuePerHour;
+            Hours = hours;
+        }
+
+        public double TotalValue()
+        {
+            return Hours * ValuePerHour;
+        }
+    }
+}
