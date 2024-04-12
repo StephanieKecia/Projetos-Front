62b417853f6b8377e2d093c49e455def38489c26
Stéphanie Kécia <118320951+StephanieKecia@users.noreply.github.com>
2024-04-12 17:06:56 -0300

    Em andamento..

--- Sistema de Gestão Escolar/GestEscolar/Formularios/Form1.cs
+++ Sistema de Gestão Escolar/GestEscolar/Formularios/Form1.cs
@@ -7,14 +7,38 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using CapaDeEntidade;
+using CapaDeNegocio;
 
 namespace Formularios
 {
     public partial class Form1 : Form
     {
+
+        ClasseNegocio clsUser = new ClasseNegocio();
+        ClasseEntidade clsEnt = new ClasseEntidade();
+        public static string usuario_nome;
+        public static string id_tipo;
+        public static string usuario_geral;
+        public static string usuario_codigo;
+
+        FrmPrincipal f = new FrmPrincipal();
+        
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Limpar()
+        {
+            textBox1.Text = "";
+            textBox2.Text = "";
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
         }
 
         private void pictureBox2_Click(object sender, EventArgs e)
